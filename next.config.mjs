a66b055092bc62749f10f53e0e1790780fe6c245
Abdelrahman <104079380+Abdelrahman7373@users.noreply.github.com>
2024-08-09 22:45:44 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          // Apply these headers to all routes
+          source: "/(.*)",
+          headers: [
+            {
+              key: "Cache-Control",
+              value: "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0",
+            },
+            {
+              key: "Pragma",
+              value: "no-cache",
+            },
+            {
+              key: "Expires",
+              value: "0",
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
