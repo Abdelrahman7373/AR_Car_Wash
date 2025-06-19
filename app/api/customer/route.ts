b2fca628de356bec53962fa4356c668566c10955
Abdelrahman <104079380+Abdelrahman7373@users.noreply.github.com>
2025-06-19 20:06:00 +0300

    Update route.ts 55554

--- app/api/customer/route.ts
+++ app/api/customer/route.ts
@@ -4,17 +4,21 @@ import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
 
-export const GET = async () => {
-    try {
-        await connectToDB();
+export const GET = async (req: Request) => {
+  const requestedFrom = req.headers.get("x-requested-from");
 
-        const customers = await Customer.find({});
+  if (requestedFrom !== "my-frontend") {
+    return NextResponse.json({ message: "Forbidden to access this content" }, { status: 403 });
+  }
 
-        
-        const response = NextResponse.json(customers, { status: 200 });
-        response.headers.set("Cache-Control", "no-store");
-        return response;
-    } catch (error) {
-        return NextResponse.json('Failed to fetch customers', {status: 500});
-    }
-}
+  try {
+    await connectToDB();
+    const customers = await Customer.find({});
+
+    const response = NextResponse.json(customers, { status: 200 });
+    response.headers.set("Cache-Control", "no-store");
+    return response;
+  } catch (error) {
+    return NextResponse.json("Failed to fetch customers", { status: 500 });
+  }
+};
