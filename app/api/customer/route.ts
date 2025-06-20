1a79c6b243dabfeabae6be09ff91c70485383cd3
Abdelrahman <104079380+Abdelrahman7373@users.noreply.github.com>
2025-06-21 02:58:30 +0300

    Update route.ts

--- app/api/customer/route.ts
+++ app/api/customer/route.ts
@@ -1,13 +1,15 @@
 import Customer from "@/models/customer";
 import { connectToDB } from "@/utils/database";
+import { sha256 } from "@/utils/hash";
 import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
 
 export const GET = async (req: Request) => {
   const requestedFrom = req.headers.get("x-requested-from");
+  const secretKey = await sha256(Math.floor(Date.now() / 4000).toString());
 
-  if (requestedFrom !== "my-frontend") {
+  if (requestedFrom !== secretKey) {
     return NextResponse.json({ message: "Forbidden to access this content" }, { status: 403 });
   }
 
