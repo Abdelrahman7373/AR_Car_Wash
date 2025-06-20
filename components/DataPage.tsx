a9d21263d6868577efaada32dc8c81fd938335dd
Abdelrahman <104079380+Abdelrahman7373@users.noreply.github.com>
2025-06-20 18:48:14 +0300

    Update DataPage.tsx

--- components/DataPage.tsx
+++ components/DataPage.tsx
@@ -1,13 +1,52 @@
+import {  useEffect, useState } from 'react';
 import CustomerCardList from './CustomerCardList';
 import Navbar from './Navbar';
 import Search from './Search';
 
+interface Customer {name: string, phoneNumber: string, carModel: string, _id: string}
+
 const DataPage = () => {
+  const [customers, setCustomers] = useState<Customer[]>([]);
+  const [searchText, setSearchText] = useState<string>("");
+
+  const fetchCustomers = async () => {
+    if (typeof window === 'undefined') return;
+
+    try {
+      const response = await fetch(`${window.location.origin}/api/customer?_=${Date.now()}`,{cache: 'no-store',headers: { 'x-requested-from': 'my-frontend' },});
+
+      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+
+      const data = await response.json();
+      setCustomers(data);
+    } catch (error) {
+      console.error('Failed to fetch customers:', error);
+      setCustomers([]);
+    }
+  };
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const callFetch = async () => {
+      if (!isMounted) return;
+      await fetchCustomers();
+    };
+
+    callFetch();
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+
+
   return (
     <div>
-      <Navbar />
-      <Search />
-      <CustomerCardList />
+      <Navbar refetchCustomers={fetchCustomers} />
+      <Search searchText={searchText} setSearchText={setSearchText} />
+      <CustomerCardList setCustomers={setCustomers} customers={customers} triggerRefresh={fetchCustomers} searchText={searchText} />
     </div>
   )
 }
